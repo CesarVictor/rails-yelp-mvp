9b4841da3ae2e561102e7860cd0e67a8f18578fa
Cesar Victor <cesarvictor@hotmail.fr>
2022-05-18 15:46:54 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'McDO', address: 'Courrs Blaguerie Stuntenberg', phone_number: '757137380', category: 'chinese')
+Restaurant.create(name: 'LE 122', address: '122, rue de Grenelle - Paris 7ème', phone_number: '757137409', category: 'italian')
+Restaurant.create(name: 'LE 41 PASTEUR', address: '41, boulevard Pasteur - Paris 15ème', phone_number: '757137408', category: 'japanese')
+Restaurant.create(name: 'AFRICAN LOUNGE', address: '20 bis, rue Jean Giraudoux - Paris 16ème', phone_number: '7406613755', category: 'french')
+Restaurant.create(name: 'AU 41 PENTHIÈVRE', address: '41, rue de Penthièvre - Paris 8ème', phone_number: '7488810909', category: 'belgian')
